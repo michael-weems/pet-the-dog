9551827cbc337fdc03aaee6f169782a15b73fbde
Michael Weems <Mweems5@gmail.Com>
2022-01-02 01:00:09 +0000

    Fix workspace issues

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+var http = require('http');  
+var fs = require('fs');  
+var server = http.createServer(function(request, response) {  
+    fs.readFile(__dirname + 'index.html', function(error, data) {  
+        if (error) {  
+            response.writeHead(404);  
+            response.write(error);  
+            response.end();  
+        } else {  
+            response.writeHead(200, {  
+                'Content-Type': 'text/html'  
+            });  
+            response.write(data);  
+            response.end();  
+        }  
+    });  
+});  
+server.listen(8082);
